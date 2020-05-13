b3db0da0d29e9b9a07eb444d416f0cbb04b65646
LokiSharp <loki.sharp@gmail.com>
2020-05-13 22:59:25 +0800

    Get executable Name in Process Command with GetApplicationArguments()

--- src/System.CommandLine/RootCommand.cs
+++ src/System.CommandLine/RootCommand.cs
@@ -1,9 +1,12 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace System.CommandLine
 {
@@ -40,14 +43,69 @@ namespace System.CommandLine
 
         private static readonly Lazy<string> _executableName = new Lazy<string>(() =>
         {
-            var location = _executablePath.Value;
-            if (string.IsNullOrEmpty(location))
-            {
-                location = Environment.GetCommandLineArgs().FirstOrDefault();
-            }
+
+            var location = GetApplicationArguments().FirstOrDefault();
             return Path.GetFileNameWithoutExtension(location).Replace(" ", "");
         });
+        
+        private static string[]? s_arguments;
 
+        private static string[] GetApplicationArguments()
+        {
+            // Environment.GetCommandLineArgs doesn't include arguments passed to the runtime.
+            // We use a native API to get all arguments.
+
+            if (s_arguments != null)
+            {
+                return s_arguments;
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                s_arguments = File.ReadAllText($"/proc/{Process.GetCurrentProcess().Id}/cmdline").Split('\0');
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                IntPtr ptr = GetCommandLine();
+                string commandLine = Marshal.PtrToStringAuto(ptr);
+                s_arguments = CommandLineToArgs(commandLine);
+            }
+            else
+            {
+                throw new PlatformNotSupportedException($"{nameof(GetApplicationArguments)} is not supported on this platform.");
+            }
+
+            return s_arguments;
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr GetCommandLine();
+
+        [DllImport("shell32.dll", SetLastError = true)]
+        private static extern IntPtr CommandLineToArgvW([MarshalAs(UnmanagedType.LPWStr)] string lpCmdLine, out int pNumArgs);
+
+        public static string[] CommandLineToArgs(string commandLine)
+        {
+            int argc;
+            var argv = CommandLineToArgvW(commandLine, out argc);
+            if (argv == IntPtr.Zero)
+                throw new Win32Exception();
+            try
+            {
+                var args = new string[argc];
+                for (var i = 0; i < args.Length; i++)
+                {
+                    var p = Marshal.ReadIntPtr(argv, i * IntPtr.Size);
+                    args[i] = Marshal.PtrToStringUni(p);
+                }
+
+                return args;
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(argv);
+            }
+        }
         private static Assembly GetAssembly() =>
             Assembly.GetEntryAssembly() ??
             Assembly.GetExecutingAssembly();
